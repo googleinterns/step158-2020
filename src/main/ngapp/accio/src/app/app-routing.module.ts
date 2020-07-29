73d6c140cbbeceeac4eef049a63add989e94e79a
Shaunna <shcaffrey@google.com>
2020-07-29 15:08:00 +0000

    add basic layout for pages

--- src/main/ngapp/accio/src/app/app-routing.module.ts
+++ src/main/ngapp/accio/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'editor', component: EditorComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/ngapp/accio/src/app/app.module.ts
+++ src/main/ngapp/accio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent} from './top-bar/top-bar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    GalleryComponent,
+    EditorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
