93cb137d296bd898ec2af314194dbc93c2d370de
Dylon <dylon.tjanaka@gmail.com>
2020-07-30 22:24:50 +0000

    Implement owner/editor check and file extension check.

--- src/main/java/com/google/sps/servlets/BlobServlet.java
+++ src/main/java/com/google/sps/servlets/BlobServlet.java
@@ -10,6 +10,8 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
@@ -19,6 +21,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.annotation.WebServlet;
@@ -39,8 +43,53 @@ public class BlobServlet extends HttpServlet {
       return;
     }
 
-    // Get the name entered by the user
-    String name = request.getParameter("name");
+    String projId = request.getParameter("proj-id");
+    String uid = userService.getCurrentUser().getUserId();
+
+    Query queryOwner = new Query("Project");
+    Query queryEditor = new Query("Project");
+
+    Filter ownerFilter = new CompositeFilter(
+        CompositeFilterOperator.AND,
+        Arrays.asList(
+            new FilterPredicate("proj-id", FilterOperator.EQUAL, projId),
+            new FilterPredicate("owners", FilterOperator.EQUAL, uid)));
+
+    Filter editorFilter = new CompositeFilter(
+        CompositeFilterOperator.AND,
+        Arrays.asList(
+            new FilterPredicate("proj-id", FilterOperator.EQUAL, projId),
+            new FilterPredicate("editors", FilterOperator.EQUAL, uid)));
+
+    queryOwner.setFilter(ownerFilter);
+    queryEditor.setFilter(editorFilter);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery ownThisProject = datastore.prepare(queryOwner);
+    PreparedQuery editThisProject = datastore.prepare(queryEditor);
+
+    Boolean isOwner = false;
+
+    if (ownThisProject.countEntities() != 0) {
+      isOwner = true;
+    } else {
+      if (editThisProject.countEntities() == 0) {
+        response.sendRedirect("/imgupload.html");
+        return;
+      }
+    }
+
+    String parentImg = request.getParameter("parent-img");
+
+    Boolean isMask = false;
+    if (parentImg != null) {
+      isMask = true;
+    }
+
+    ArrayList<String> validExtensions = new ArrayList<String>(
+        (isMask) ? Arrays.asList("png")
+                 : Arrays.asList("png", "jpg", "jpeg", "jfif", "pjpeg", "pjp",
+                                 "gif", "bmp", "ico", "cur", "svg", "webp"));
 
     BlobstoreService blobstoreService =
         BlobstoreServiceFactory.getBlobstoreService();
@@ -49,39 +98,34 @@ public class BlobServlet extends HttpServlet {
 
     // User submitted form without selecting a file
     if (blobKeys == null || blobKeys.isEmpty()) {
+      response.sendRedirect("/imgupload.html");
       return;
     }
 
-    // Our form only contains a single file input
-    BlobKey blobKey = blobKeys.get(0);
+    // Check validity of each blob key
+    Iterator<BlobKey> itr = blobKeys.iterator();
+    while (itr.hasNext()) {
+      BlobKey blobKey = itr.next();
+      BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+      String[] splitFilename = blobInfo.getFilename().split(".");
+      String extension = splitFilename[splitFilename.length - 1].toLowerCase();
+      if (blobInfo.getSize() == 0 || !validExtensions.contains(extension)) {
+        blobstoreService.delete(blobKey);
+        itr.remove();
+      }
+    }
 
-    // Check for valid blob key
-    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-    if (blobInfo.getSize() == 0) {
-      blobstoreService.delete(blobKey);
+    // None of the keys were valid
+    if (blobKeys.isEmpty()) {
+      response.sendRedirect("/imgupload.html");
       return;
     }
-    String uid = userService.getCurrentUser().getUserId();
-    Query query = new Query("User");
-    Filter propertyFilter =
-        new FilterPredicate("uid", FilterOperator.EQUAL, uid);
 
-    query.setFilter(propertyFilter);
+    // Get the name entered by the user
+    String name = request.getParameter("name");
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery user = datastore.prepare(query);
-
-    Entity userEntity = new Entity("User");
-
-    if (user.countEntities() == 0) {
-      userEntity.setProperty("uid", uid);
-      datastore.put(userEntity);
-    } else {
-      userEntity = user.asSingleEntity();
-    }
-
-    Entity imageEntity = new Entity("Image", userEntity.getKey());
-    imageEntity.setProperty("blobkey", blobKey.getKeyString());
+    Entity imageEntity = new Entity("Image"/*, userEntity.getKey()*/);
+    // imageEntity.setProperty("blobkey", blobKey.getKeyString());
     imageEntity.setProperty("name", name);
     datastore.put(imageEntity);
 
@@ -118,11 +162,13 @@ public class BlobServlet extends HttpServlet {
     ArrayList<String> blobKeys = new ArrayList<String>();
 
     for (Entity entity : storedBlobKeys.asIterable()) {
-      String url = "/blob-host?blobkey=" + (String)entity.getProperty("blobkey");  
+      String url =
+          "/blob-host?blobkey=" + (String)entity.getProperty("blobkey");
       blobKeys.add(url);
     }
 
-    Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    Gson gson =
+        new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
     String jsonBlobKeys = gson.toJson(blobKeys);
     response.getWriter().println(jsonBlobKeys);
   }
