cea393292969d8388de0139f197b5ea9a4a76306
Shaunna <shcaffrey@google.com>
2020-07-31 19:27:39 +0000

    update top-bar

--- src/main/ngapp/accio/src/app/top-bar/top-bar.component.ts
+++ src/main/ngapp/accio/src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Directive } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
+
+
 export class TopBarComponent implements OnInit {
+
   constructor() { }
 
   userSignedIn = false;
@@ -19,9 +22,11 @@ export class TopBarComponent implements OnInit {
       this.userSignedIn = true;
       return signIn;
     }
-    console.log('User Signed out');
-    this.userSignedIn = false;
-    return signOut;
+    else {
+      console.log('User Signed out');
+      this.userSignedIn = false;
+      return signOut;
+    }
   }
 }
 
