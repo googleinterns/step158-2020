2b217e3f2f1cafa82c8c213eced798f6d431b975
shmcaffrey <45078226+shmcaffrey@users.noreply.github.com>
2020-07-31 10:59:14 -0400

    update ngOnInit in editor.ts

--- src/main/ngapp/accio/src/app/editor/editor.component.ts
+++ src/main/ngapp/accio/src/app/editor/editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { imageUrls } from '../images';
 
@@ -21,17 +21,29 @@ export class EditorComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     //returns url user clicked in gallery component
     this.route.paramMap.subscribe(params => {
-      this.url = imageUrls[+params.get('imgUrl')];
-
-     this.ctx = this.canvas.nativeElement.getContext('2d');
-     this.draw();
+      let index = Number(params.get('imgUrl'));
+      this.checkImageUrl(index);
+      this.url = imageUrls[index];
     });
+
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.draw();
   }  
+  
+  //  Checks that route for editor URL is in bounds of array of Urls and is a number
+  //  Redirects to gallery component if not
+  private checkImageUrl(index: number) {
+    if (Number.isNaN(index) || index >= imageUrls.length) {    
+      console.log('Index: ' + index + 'Not a number or greater than number of images, returning to Gallery');
+      this.router.navigate(['/gallery']);
+    }
+  }
   
   // Draws the image user selects from gallery on Canvas
   // Currently, editor can require user to scroll to access
