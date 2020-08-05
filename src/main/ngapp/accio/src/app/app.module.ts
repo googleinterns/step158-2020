a1f140a790a86c51865f6e3a82e4ec89e8150add
Shaunna <shcaffrey@google.com>
2020-08-05 19:33:53 +0000

    refactor gallery to img-gallery

--- src/main/ngapp/accio/src/app/app.module.ts
+++ src/main/ngapp/accio/src/app/app.module.ts
@@ -4,19 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent} from './top-bar/top-bar.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { ImgGalleryComponent } from './img-gallery/img-gallery.component';
 import { EditorComponent } from './editor/editor.component';
 import { HomeComponent } from './home/home.component';
 import { MaskDirective } from './mask.directive';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    GalleryComponent,
+    ImgGalleryComponent,
     EditorComponent,
     HomeComponent,
     MaskDirective,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
