dcaac730db4ba71eca92d77150e8049254c2ee66
chance <laufate@gmail.com>
2020-08-06 22:59:23 +0000

    Give path to top-bar redirection

--- src/main/ngapp/accio/src/app/app-routing.module.ts
+++ src/main/ngapp/accio/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { EditorComponent } from './editor/editor.component';
 import { HomeComponent } from './home/home.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 const routes: Routes = [
   { path: 'gallery', component: GalleryComponent },
   { path: 'editor', component: EditorComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: ':buttonLink', component: TopBarComponent}
 ];
 
 @NgModule({
