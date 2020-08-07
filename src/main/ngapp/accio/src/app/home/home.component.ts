60e1658396635c44e96c3cece1ddd92ebb85c2f5
chance <laufate@gmail.com>
2020-08-07 15:14:59 +0000

    Add project creation and intit proj fetch list

--- src/main/ngapp/accio/src/app/home/home.component.ts
+++ src/main/ngapp/accio/src/app/home/home.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // TODO: add filter global once talk w/ DB team to understand it.
+  // Also add it as attribute in getProject's url var.
+  filterVisibility: string;
+  filterRole: string;
+  filterTag: string;
+  filterId: string;
+  filterSort: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  async getProjects(): Promise<any> {
+    let url = '/projects?' + $.param({
+      'visibility': this.filterVisibility,
+      'role': this.filterRole,
+      'search-term': this.filterTag,
+      'sort': this.filterSort,
+      'proj-id': this.filterId
+    });
+    const response = await fetch(url);
+    /**@returns a list of objects:
+     * {name: string,
+     * projId: string,
+     * timestamp: string,
+     * visibility: string,
+     * owners: list<string>,
+     * editors: list<string>}
+     */
+    const content = await response.json();
+
+    const contentListElement = document.getElementById("list-container");
+    contentListElement.innerHTML = '';
+    for (let i = 0; i < content.length; i++) {
+      contentListElement.appendChild(
+          this.createListElement(content['name'], content['timestamp'])
+          );
+    }
+  }
+
+  createListElement(name: string, timestamp: string): HTMLLIElement {
+    const liElement = document.createElement('li');
+    liElement.innerText = name + '\n' + timestamp;
+    return liElement;
+  }
 }
