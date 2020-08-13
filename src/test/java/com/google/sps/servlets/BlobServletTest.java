0ce95dc5d8a67b7b6ab549c619a4b13f073e1750
Dylon <dylon.tjanaka@gmail.com>
2020-08-13 04:24:43 +0000

    Add dependencies and skeleton for servlet and datastore testing.

---
+++ src/test/java/com/google/sps/servlets/BlobServletTest.java
@@ -0,0 +1,62 @@
+package com.google.sps.servlets;
+
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+import static org.junit.Assert.assertEquals;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+@RunWith(JUnit4.class)
+public final class BlobServletTest {
+
+  private BlobServlet servlet;
+  private MockHttpServletRequest request;
+  private MockHttpServletResponse response;
+
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+    servlet = new BlobServlet();
+    request = new MockHttpServletRequest();
+    response = new MockHttpServletResponse();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  // Run this test twice to prove we're not leaking any state across tests.
+  private void doTest() {
+    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+    assertEquals(0, ds.prepare(new Query("yam")).countEntities(withLimit(10)));
+    ds.put(new Entity("yam"));
+    ds.put(new Entity("yam"));
+    assertEquals(2, ds.prepare(new Query("yam")).countEntities(withLimit(10)));
+  }
+
+  @Test
+  public void testInsert1() {
+    doTest();
+  }
+
+  @Test
+  public void testInsert2() {
+    doTest();
+  }
+}
