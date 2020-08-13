0010e38e2b7548e8723358c82414bbc5c130cf77
shmcaffrey <45078226+shmcaffrey@users.noreply.github.com>
2020-08-13 08:47:51 -0400

    edit to check for empty Image name parameter

--- src/main/ngapp/accio/src/app/img-gallery/img-gallery.component.ts
+++ src/main/ngapp/accio/src/app/img-gallery/img-gallery.component.ts
@@ -98,6 +98,11 @@ export class ImgGalleryComponent implements OnInit {
   *         deleteIn: string = 'delete'
   */
   onSubmit() {
+     // Name is a required input. If it's null, do nothing.
+    if(!this.uploadImageForm.get('imgName').value) {
+      return;
+    }
+    
     //  uploadImageForm 'image' contains a file, so the value is a file array.
     //  To serve the blob we have to access the first file in the array.
     const fileArray = this.uploadImageForm.get('image').value;
