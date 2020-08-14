73b713dccc002a0929d7a4a05a0eb6bfeda52480
Dylon <dylon.tjanaka@gmail.com>
2020-08-14 05:23:00 +0000

    Move blobs test constants to separate file.

---
+++ src/test/java/com/google/sps/BlobServletTestUtils.java
@@ -0,0 +1,59 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import java.util.Arrays;
+
+public final class BlobServletTestUtils {
+  public static final String expectedNoFilters = "[\n  {\n    \"url\": \"/blob-host?blobkey=def\",\n    \"name\": \"Image1\",\n    \"utc\": \"2020-08-12T05:39:02.384Z\",\n    \"tags\": [\n      \"1\",\n      \"one\"\n    ],\n    \"masks\": []\n  },\n  {\n    \"url\": \"/blob-host?blobkey=abc\",\n    \"name\": \"Image0\",\n    \"utc\": \"2020-08-12T05:39:02.383Z\",\n    \"tags\": [\n      \"0\",\n      \"zero\"\n    ],\n    \"masks\": []\n  }\n]\n";
+
+  public static String databaseSetup() {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    Entity projEntity = new Entity(DataUtils.PROJECT, 123456);
+    projEntity.setProperty("name", "MyProject");
+    projEntity.setProperty("utc", "2020-08-12T05:39:02.383Z");
+    projEntity.setProperty("visibility", DataUtils.PRIVATE);
+    projEntity.setIndexedProperty("owners",
+                                  Arrays.asList("abc@xyz.com", "def@uvw.com"));
+    projEntity.setIndexedProperty("editors",
+                                  Arrays.asList("xyz@abc.com", "uvw@def.com"));
+    Key projKey = datastore.put(projEntity);
+    String projId = KeyFactory.keyToString(projKey);
+
+    Entity imgEntity = new Entity(DataUtils.IMAGE, 1, projKey);
+    imgEntity.setProperty("name", "Image0");
+    imgEntity.setProperty("utc", "2020-08-12T05:39:02.383Z");
+    imgEntity.setProperty("blobkey", "abc");
+    imgEntity.setIndexedProperty("tags", Arrays.asList("0", "zero"));
+
+    Entity imgEntity2 = new Entity(DataUtils.IMAGE, 2, projKey);
+    imgEntity2.setProperty("name", "Image1");
+    imgEntity2.setProperty("utc", "2020-08-12T05:39:02.384Z");
+    imgEntity2.setProperty("blobkey", "def");
+    imgEntity2.setIndexedProperty("tags", Arrays.asList("1", "one"));
+
+    datastore.put(Arrays.asList(imgEntity, imgEntity2));
+
+    Entity maskEntity = new Entity(DataUtils.MASK, 1, imgEntity.getKey());
+    maskEntity.setProperty("name", "Mask0");
+    maskEntity.setProperty("utc", "2020-08-12T05:39:02.384Z");
+    maskEntity.setProperty("blobkey", "ghi");
+    maskEntity.setIndexedProperty("tags", Arrays.asList("0", "zero"));
+
+    Entity maskEntity2 = new Entity(DataUtils.MASK, 2, imgEntity.getKey());
+    maskEntity2.setProperty("name", "Mask1");
+    maskEntity2.setProperty("utc", "2020-08-12T05:39:02.383Z");
+    maskEntity2.setProperty("blobkey", "jkl");
+    maskEntity2.setIndexedProperty("tags", Arrays.asList("1", "one"));
+
+    datastore.put(Arrays.asList(maskEntity, maskEntity2));
+
+    return projId;
+  }
+
+  private BlobServletTestUtils() {}
+}
--- src/test/java/com/google/sps/servlets/BlobServletTest.java
+++ src/test/java/com/google/sps/servlets/BlobServletTest.java
@@ -2,12 +2,10 @@ package com.google.sps.servlets;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static com.google.sps.servlets.BlobServletTestUtils.*;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.appengine.tools.development.testing.LocalBlobstoreServiceTestConfig;
@@ -43,55 +41,12 @@ public final class BlobServletTest {
   public void setUp() {
     helper.setUp();
     servlet = new BlobServlet();
-    databaseSetup();
+    projId = databaseSetup();
   }
 
   @After
   public void tearDown() {
     helper.tearDown();
-  }
-
-  private void databaseSetup() {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-    Entity projEntity = new Entity(DataUtils.PROJECT, 123456);
-    projEntity.setProperty("name", "MyProject");
-    projEntity.setProperty("utc", "2020-08-12T05:39:02.383Z");
-    projEntity.setProperty("visibility", DataUtils.PRIVATE);
-    projEntity.setIndexedProperty("owners",
-                                  Arrays.asList("abc@xyz.com", "def@uvw.com"));
-    projEntity.setIndexedProperty("editors",
-                                  Arrays.asList("xyz@abc.com", "uvw@def.com"));
-    Key projKey = datastore.put(projEntity);
-    projId = KeyFactory.keyToString(projKey);
-
-    Entity imgEntity = new Entity(DataUtils.IMAGE, 1, projKey);
-    imgEntity.setProperty("name", "Image0");
-    imgEntity.setProperty("utc", "2020-08-12T05:39:02.383Z");
-    imgEntity.setProperty("blobkey", "abc");
-    imgEntity.setIndexedProperty("tags", Arrays.asList("0", "zero"));
-
-    Entity imgEntity2 = new Entity(DataUtils.IMAGE, 2, projKey);
-    imgEntity2.setProperty("name", "Image1");
-    imgEntity2.setProperty("utc", "2020-08-12T05:39:02.384Z");
-    imgEntity2.setProperty("blobkey", "def");
-    imgEntity2.setIndexedProperty("tags", Arrays.asList("1", "one"));
-
-    datastore.put(Arrays.asList(imgEntity, imgEntity2));
-
-    Entity maskEntity = new Entity(DataUtils.MASK, 1, imgEntity.getKey());
-    maskEntity.setProperty("name", "Mask0");
-    maskEntity.setProperty("utc", "2020-08-12T05:39:02.384Z");
-    maskEntity.setProperty("blobkey", "ghi");
-    maskEntity.setIndexedProperty("tags", Arrays.asList("0", "zero"));
-
-    Entity maskEntity2 = new Entity(DataUtils.MASK, 2, imgEntity.getKey());
-    maskEntity2.setProperty("name", "Mask1");
-    maskEntity2.setProperty("utc", "2020-08-12T05:39:02.383Z");
-    maskEntity2.setProperty("blobkey", "jkl");
-    maskEntity2.setIndexedProperty("tags", Arrays.asList("1", "one"));
-
-    datastore.put(Arrays.asList(maskEntity, maskEntity2));
   }
 
   @Test
@@ -107,10 +62,8 @@ public final class BlobServletTest {
     when(response.getWriter()).thenReturn(writer);
     servlet.doGet(request, response);
     writer.flush();
-
-    String expected =
-        "[\n  {\n    \"url\": \"/blob-host?blobkey=def\",\n    \"name\": \"Image1\",\n    \"utc\": \"2020-08-12T05:39:02.384Z\",\n    \"tags\": [\n      \"1\",\n      \"one\"\n    ],\n    \"masks\": []\n  },\n  {\n    \"url\": \"/blob-host?blobkey=abc\",\n    \"name\": \"Image0\",\n    \"utc\": \"2020-08-12T05:39:02.383Z\",\n    \"tags\": [\n      \"0\",\n      \"zero\"\n    ],\n    \"masks\": []\n  }\n]\n";
-    assertEquals(expected, stringWriter.toString());
+        
+    assertEquals(expectedNoFilters, stringWriter.toString());
   }
 
   // doget
