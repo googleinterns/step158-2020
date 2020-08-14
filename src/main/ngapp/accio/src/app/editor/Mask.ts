57ad93cb1892b05e8f3ecc0be21ea7effc4a41f1
chance <laufate@gmail.com>
2020-08-14 15:33:21 +0000

    Finish setting up skeleton code for Mask class

--- src/main/ngapp/accio/src/app/editor/Mask.ts
+++ src/main/ngapp/accio/src/app/editor/Mask.ts
@@ -9,6 +9,7 @@ export class Mask {
   // Cannot access mask directly, so that a mask can never be accidentally
   // altered without also changing its history.
   getMask(): Set<number> {
+    // Change this implementation if undo/redo design is changed
     return this.mask;
   }
 
@@ -21,4 +22,57 @@ export class Mask {
   }
 
   undo() {/* TODO */}
+}
+
+// Class to perform basic set operations
+class SetOperator {
+  // Ie. [1, 2, 3] union [3, 4, 5] ==> [1, 2, 3, 4, 5]
+  static union(setA: Set<number>, setB: Set<number>) {
+    let result = new Set(setA);
+
+    for (let elem of setB) {
+      result.add(elem);
+    }
+
+    return result;
+  }
+  
+  // Ie. [1, 2, 3] intersect [3, 4, 5] ==> [3]
+  static intersection(setA: Set<number>, setB: Set<number>) {
+    let result = new Set();
+
+    for (let elem of setB) {
+      if (setA.has(elem)) {
+        result.add(elem);
+      }
+    }
+
+    return result;
+  }
+
+  // Ie. [1, 2, 3] difference [3, 4, 5] ==> [1, 2]
+  static difference(setA: Set<number>, setB: Set<number>) {
+    let result = new Set(setA);
+
+    for (let elem of setB) {
+      result.delete(elem);
+    }
+
+    return result;
+  }
+
+  // Ie. [1, 2, 3] symmetricDifference [3, 4, 5] ==> [1, 2, 4, 5]
+  static symmetricDifference(setA: Set<number>, setB: Set<number>) {
+    let result = new Set(setA);
+
+    for (let elem of setB) {
+      if (result.has(elem)) {
+        result.delete(elem);
+      } else {
+        result.add(elem);
+      }
+    }
+
+    return result;
+  }
 }
