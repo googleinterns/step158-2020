53b0b4fe7687af36fbf062ce619ba1853383f8e3
chance <laufate@gmail.com>
2020-08-17 21:03:53 +0000

    Set up scribble flood fill algo

--- src/main/ngapp/accio/src/app/editor/magic-wand.service.ts
+++ src/main/ngapp/accio/src/app/editor/magic-wand.service.ts
@@ -175,4 +175,84 @@ export class MagicWandService {
 
     return invertedMask;
   }
+
+
+  /* Smarter flood fill tool */
+
+  scribbleFloodfill(imgData: ImageData, xCoord: number, yCoord: number,
+      tolerance: number, sribbles: Set<number>) {
+    /**Store a queue of coords for pixels that we need to visit in "visit".
+     * Store already-visited pixels in "visited" as index formatted numbers
+     * (as opposed to coord format; for Set funcs).
+     */
+    const visit: Array<Array<number>> = new Array();
+    const visited: Set<number> = new Set();
+    // Use a set for mask; mainly do iter and set operations on masks
+    const mask: Set<number> = new Set();
+    // Represent [R,G,B,A] attributes of initial pixel
+    const originalPixel: Array<number> =
+        this.dataArrayToRgba(imgData, xCoord, yCoord);
+
+    visit.push([xCoord, yCoord]);
+    // Convert [x,y] format coord to 1-D equivalent of imgData.data (DataArray)
+    const indexAsDataArray: number =
+        this.coordToDataArrayIndex(xCoord, yCoord, imgData.width);
+    visited.add(indexAsDataArray);
+
+    // Loop until no more adjacent pixels within tolerance level
+    while (visit.length !== 0) {
+      const coord: Array<number> = visit.pop();
+      // Unpack coord
+      const x: number = coord[0];
+      const y: number = coord[1];
+
+      // Operational part of while-loop
+      mask.add(this.coordToDataArrayIndex(x, y, imgData.width));
+
+      // Loop part of while-loop
+
+      // Get coords of adjacent pixels
+      const neighbors: Array<Array<number>> =
+          [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]];
+      // Add coords of adjacent pixels to the heap
+      for (const neighborPixel of neighbors) {
+        const x: number = neighborPixel[0];
+        const y: number = neighborPixel[1];
+        // Check if coord is in bounds and has not been visited first
+        if (!this.getIsValid(imgData.width, imgData.height, x, y, visited)) {
+          continue;
+        }
+        // Visit the pixel and check if it should be part of the mask
+        visited.add(this.coordToDataArrayIndex(x, y, imgData.width));
+        if (this.getIsScribbleMask(originalPixel, imgData, neighborPixel, tolerance)) {
+          visit.push(neighborPixel);
+        }
+      }
+    }  // end of while loop
+
+    return mask;
+  }
+
+  /**Judge current pixel's RGBA against original pixel's RGBA to
+   * see if it can still be part of the mask (using tolerance criteria).
+   */
+  getIsScribbleMask(originalPixel: Array<number>, imgData: ImageData,
+      pixelCoord: Array<number>, tolerance: number): boolean {
+    const curX: number = pixelCoord[0];
+    const curY: number = pixelCoord[1];
+
+    // Get array of attributes of current pixel
+    const curPixel: Array<number> = this.dataArrayToRgba(imgData, curX, curY);
+
+    // All attributes of the pixel (R,G,B, and A) must be within tolerance level
+    for (let i = 0; i < 4; i++) {
+      const upperTolerance: boolean = curPixel[i] > originalPixel[i] + tolerance;
+      const lowerTolerance: boolean = curPixel[i] < originalPixel[i] - tolerance;
+      if (upperTolerance || lowerTolerance) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
