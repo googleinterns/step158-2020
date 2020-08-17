e1436f395916472f49bfe443ca8642880e8e1ce4
chance <laufate@gmail.com>
2020-08-17 23:28:14 +0000

    Euclidean dist, comments - Change tolerance evaluation to use euclidean distance - Refactor all comments to be present tense grammar

--- src/main/ngapp/accio/src/app/editor/magic-wand.service.ts
+++ src/main/ngapp/accio/src/app/editor/magic-wand.service.ts
@@ -7,65 +7,65 @@ import { SetOperator } from './set-operator';
 })
 export class MagicWandService {
   /**4-Way floodfill (left, right, up, down):
-   * Return set of coordinates(formatted as a 1-D array).
+   * @returns {Set<number>} set of coordinates(formatted as a 1-D array).
    * Coordinates correspond to pixels considered part of the mask.
    */
   /* tslint:disable */
   floodfill(imgData: ImageData, xCoord: number, yCoord: number,
       tolerance: number): Set<number> {
-    /**Store a queue of coords for pixels that we need to visit in "visit".
-     * Store already-visited pixels in "visited" as index formatted numbers
-     * (as opposed to coord format; for Set funcs).
-     */
+    // Stores a queue of coords for pixels that we need to visit in "visit".
     const visit: Array<Array<number>> = new Array();
+    // Stores already-visited pixels in "visited" as index formatted numbers
+    // (as opposed to coord format; for Set funcs).
     const visited: Set<number> = new Set();
-    // Use a set for mask; mainly do iter and set operations on masks
+    // Uses a set for mask; mainly do iter and set operations on masks...
     const mask: Set<number> = new Set();
-    // Represent [R,G,B,A] attributes of initial pixel
+    // Represents [R,G,B] attributes of initial pixel.
     const originalPixel: Array<number> =
         this.dataArrayToRgba(imgData, xCoord, yCoord);
 
     visit.push([xCoord, yCoord]);
-    // Convert [x,y] format coord to 1-D equivalent of imgData.data (DataArray)
+    // Converts [x,y] format coord to 1-D equivalent of
+    // imgData.data (DataArray).
     const indexAsDataArray: number =
         this.coordToDataArrayIndex(xCoord, yCoord, imgData.width);
     visited.add(indexAsDataArray);
 
-    // Loop until no more adjacent pixels within tolerance level
+    // Loops until no more adjacent pixels within tolerance level.
     while (visit.length !== 0) {
       const coord: Array<number> = visit.pop();
-      // Unpack coord
+      // Unpacks coord
       const x: number = coord[0];
       const y: number = coord[1];
 
-      // Operational part of while-loop
+      // Operational part of while-loop...
       mask.add(this.coordToDataArrayIndex(x, y, imgData.width));
 
-      // Loop part of while-loop
+      // Loop part of while-loop...
 
-      // Get coords of adjacent pixels
+      // Gets coords of adjacent pixels.
       const neighbors: Array<Array<number>> =
           [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]];
-      // Add coords of adjacent pixels to the heap
+      // Adds coords of adjacent pixels to the heap.
       for (const neighborPixel of neighbors) {
         const x: number = neighborPixel[0];
         const y: number = neighborPixel[1];
-        // Check if coord is in bounds and has not been visited first
+        // Checks if coord is in bounds and has not been visited first.
         if (!this.getIsValid(imgData.width, imgData.height, x, y, visited)) {
           continue;
         }
-        // Visit the pixel and check if it should be part of the mask
+        // Visits the pixel and checks if it should be part of the mask.
         visited.add(this.coordToDataArrayIndex(x, y, imgData.width));
         if (this.getIsMask(originalPixel, imgData, neighborPixel, tolerance)) {
           visit.push(neighborPixel);
         }
       }
-    }  // end of while loop
+    }  // End of while loop...
 
     return mask;
   }
 
-  /**Judge current pixel's RGBA against original pixel's RGBA to
+  /**Judges current pixel's RGB against original pixel's RGB to
    * see if it can still be part of the mask (using tolerance criteria).
    */
   getIsMask(originalPixel: Array<number>, imgData: ImageData,
@@ -73,11 +73,11 @@ export class MagicWandService {
     const curX: number = pixelCoord[0];
     const curY: number = pixelCoord[1];
 
-    // Get array of attributes of current pixel
+    // Gets array of the color attributes of current pixel.
     const curPixel: Array<number> = this.dataArrayToRgba(imgData, curX, curY);
 
-    // All attributes of the pixel (R,G,B, and A) must be within tolerance level
-    for (let i = 0; i < 4; i++) {
+    // Color attributes of pixel (R,G, and B) must be within tolerance level.
+    for (let i = 0; i < 3; i++) {
       const upperTolerance: boolean = curPixel[i] > originalPixel[i] + tolerance;
       const lowerTolerance: boolean = curPixel[i] < originalPixel[i] - tolerance;
       if (upperTolerance || lowerTolerance) {
@@ -88,14 +88,38 @@ export class MagicWandService {
     return true;
   }
 
-  // Check if @pixelCoord is in bounds and makes sure it's not a repeat coord
+  /**@returns {number} the straight line distance between the two colors
+   * @param {Array<number> [R, G, B]} basisColor and 
+   * @param {Array<number> [R, G, B]} secondColor
+   */
+  RgbEuclideanDist(basisColor: Array<number>, secondColor: Array<number>)
+      : number {
+    if (basisColor.length != secondColor.length) {
+      throw new Error(
+          'basisColor and secondColor must be same lengthed arrays...');
+    }
+    if (basisColor.length != 3) {
+      throw new Error(
+          'Arguments must be an array of [R, G, B] (length == 3)...');
+    }
+
+    let distance = 0;
+
+    for (let i = 0; i < basisColor.length; i++) {
+      distance += Math.pow((basisColor[i] - secondColor[i]), 2);
+    }
+
+    return Math.sqrt(distance);
+  } 
+
+  // Checks if @pixelCoord is in bounds and makes sure it's not a repeat coord.
   getIsValid(imgWidth: number, imgHeight: number, curX: number, curY: number,
       visited: Set<number>): boolean {
     return this.isInBounds(imgWidth, imgHeight, curX, curY) &&
         this.notVisited(imgWidth, curX, curY, visited);
   }
 
-  // Check bounds of indexing for img dimensions
+  // Checks bounds of indexing for img dimensions.
   isInBounds(imgWidth: number, imgHeight: number, curX: number, curY: number):
       boolean {
     let yInBounds: boolean = curY >= 0 && curY < imgHeight;
@@ -104,10 +128,10 @@ export class MagicWandService {
     return yInBounds && xInBounds;
   }
 
-  // Checks if pixel has been visited already
+  // Checks if pixel has been visited already.
   notVisited(imgWidth: number, curX: number, curY: number,
       visited: Set<number>): boolean {
-    // Do not push repeat coords to heap
+    // Do not push repeating coords to heap...
     const index: number =
         this.coordToDataArrayIndex(curX, curY, imgWidth);
 
@@ -115,29 +139,32 @@ export class MagicWandService {
   }
 
 
-  // Return pixel attributes of @imgData at [@xCoord, @yCoord] as [R, G, B, A]
+  /**@returns {Array<number> [R, G, B]} color attribute of the pixel at 
+   * @param {number} xCoord and 
+   * @param {number} yCoord based off of the original image supplied by
+   * @param {ImageData} imgData
+   */
   dataArrayToRgba(imgData: ImageData, xCoord: number, yCoord: number):
       Array<number> {
-    // Unpack imgData for readability
+    // Unpacks imgData for readability.
     const data: Uint8ClampedArray = imgData.data;
     const imgWidth: number = imgData.width;
 
-    // Pixel attributes in imgData are organized adjacently in a 1-D array
+    // Pixel attributes in imgData are organized adjacently in a 1-D array.
     const pixelIndex: number =
         this.coordToDataArrayIndex(xCoord, yCoord, imgWidth);
     const red: number = data[pixelIndex];
     const green: number = data[pixelIndex + 1];
     const blue: number = data[pixelIndex + 2];
-    const alpha: number = data[pixelIndex + 3];
-    // Store original pixel's attributes
-    return [red, green, blue, alpha];
+    
+    return [red, green, blue];
   }
 
-  // Convert coord [@x, @y] (2-D) to indexing style of DataArray (1-D)
-  /**Returns index of start of pixel at @x, @y
+  // Converts coord [@x, @y] (2-D) to indexing style of DataArray (1-D)
+  /**@returns {number} index of the start of the pixel at
+   * @param {number} x and 
+   * @param {number} y 
    * (which represents the red attribute of that pixel)
-   * Important: Returns array of number representing indices.
-   * First element contains value of index for attribute: red and so on
    */
   coordToDataArrayIndex(x: number, y: number, width: number): number {
     return (x + (y * width)) * 4;
@@ -148,7 +175,8 @@ export class MagicWandService {
 
   /**@returns {Set<number>} mask that excludes the
    * @param {Set<number>} mistake from original
-   * @param {Set<number>} mask*/
+   * @param {Set<number>} mask
+   */
   erase(mask: Set<number>, mistake: Set<number>): Set<number> {
     return SetOperator.difference(mask, mistake);
   }
@@ -157,7 +185,8 @@ export class MagicWandService {
    * @param {Set<number>} originalMask .
    * Relative container that encompasses @originalMask is based on
    * @param {number} height and 
-   * @param {number} width */
+   * @param {number} width 
+   */
   invert(originalMask: Set<number>, width: number, height: number)
       : Set<number> {
     let invertedMask: Set<number> = new Set();
@@ -180,72 +209,74 @@ export class MagicWandService {
   /* Smarter flood fill tool */
 
   scribbleFloodfill(imgData: ImageData, xCoord: number, yCoord: number,
-      tolerance: number, sribbles: Set<number>) {
-    /**Store a queue of coords for pixels that we need to visit in "visit".
-     * Store already-visited pixels in "visited" as index formatted numbers
-     * (as opposed to coord format; for Set funcs).
-     */
+      tolerance: number, scribbles: Set<number>): Set<number> {
+    // Stores a queue of coords for pixels that we need to visit in "visit".
     const visit: Array<Array<number>> = new Array();
+    // Stores already-visited pixels in "visited" as index formatted numbers
+    // (as opposed to coord format; for Set funcs).
     const visited: Set<number> = new Set();
-    // Use a set for mask; mainly do iter and set operations on masks
+    // Uses a set for mask; mainly do iter and set operations on masks.
     const mask: Set<number> = new Set();
-    // Represent [R,G,B,A] attributes of initial pixel
+    // Represents [R,G,B] attributes of initial pixel.
     const originalPixel: Array<number> =
         this.dataArrayToRgba(imgData, xCoord, yCoord);
 
     visit.push([xCoord, yCoord]);
-    // Convert [x,y] format coord to 1-D equivalent of imgData.data (DataArray)
+    // Converts [x,y] format to 1-D equivalent of imgData.data (DataArray).
     const indexAsDataArray: number =
         this.coordToDataArrayIndex(xCoord, yCoord, imgData.width);
     visited.add(indexAsDataArray);
 
-    // Loop until no more adjacent pixels within tolerance level
+    // Filters scribbles by removing unnecessary pixels
+    scribbles = filterScribbles(scribbles, tolerance);
+
+    // Loops until no more adjacent pixels within tolerance level.
     while (visit.length !== 0) {
       const coord: Array<number> = visit.pop();
-      // Unpack coord
+      // Unpacks coord.
       const x: number = coord[0];
       const y: number = coord[1];
 
-      // Operational part of while-loop
+      // Operational part of while-loop...
       mask.add(this.coordToDataArrayIndex(x, y, imgData.width));
 
-      // Loop part of while-loop
+      // Loop part of while-loop...
 
-      // Get coords of adjacent pixels
+      // Gets coords of adjacent pixels.
       const neighbors: Array<Array<number>> =
           [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]];
-      // Add coords of adjacent pixels to the heap
+      // Adds coords of adjacent pixels to the heap.
       for (const neighborPixel of neighbors) {
         const x: number = neighborPixel[0];
         const y: number = neighborPixel[1];
-        // Check if coord is in bounds and has not been visited first
+        // Checks if coord is in bounds and has not been visited first.
         if (!this.getIsValid(imgData.width, imgData.height, x, y, visited)) {
           continue;
         }
-        // Visit the pixel and check if it should be part of the mask
+        // Visits the pixel and check if it should be part of the mask.
         visited.add(this.coordToDataArrayIndex(x, y, imgData.width));
         if (this.getIsScribbleMask(originalPixel, imgData, neighborPixel, tolerance)) {
           visit.push(neighborPixel);
         }
       }
-    }  // end of while loop
+    }  // End of while loop...
 
     return mask;
   }
 
-  /**Judge current pixel's RGBA against original pixel's RGBA to
+  /**Judges current pixel's RGB against original pixel's RGB to
    * see if it can still be part of the mask (using tolerance criteria).
    */
   getIsScribbleMask(originalPixel: Array<number>, imgData: ImageData,
-      pixelCoord: Array<number>, tolerance: number): boolean {
+      pixelCoord: Array<number>, tolerance: number, scribbles: Set<number>): boolean {
     const curX: number = pixelCoord[0];
     const curY: number = pixelCoord[1];
 
-    // Get array of attributes of current pixel
+    // Gets array of color attributes of current pixel.
     const curPixel: Array<number> = this.dataArrayToRgba(imgData, curX, curY);
 
-    // All attributes of the pixel (R,G,B, and A) must be within tolerance level
-    for (let i = 0; i < 4; i++) {
+    // Color attributes of pixel (R,G, and B) must be within tolerance level.
+    for (let i = 0; i < 3; i++) {
       const upperTolerance: boolean = curPixel[i] > originalPixel[i] + tolerance;
       const lowerTolerance: boolean = curPixel[i] < originalPixel[i] - tolerance;
       if (upperTolerance || lowerTolerance) {
@@ -254,5 +285,18 @@ export class MagicWandService {
     }
 
     return true;
+  }
+
+  filterScribbles(scribbles: Set<number>, tolerance: number): Set<number> {
+    // TODO
+    let result: Set<number> = new Set();
+    for (let pixelCoord of scribbles) {
+      pixelColor = RgbEuclideanDist();
+      if (/* filter condition */) {
+        result.add(pixelCoord);
+      }
+    }
+
+    return result;
   }
 }
