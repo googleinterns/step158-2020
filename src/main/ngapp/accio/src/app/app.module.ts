e8c0ac0fde5d9116f6ebff43510dc3d4122ec42d
Dylon <dylon.tjanaka@gmail.com>
2020-08-21 04:44:52 +0000

    Add download menu.

--- src/main/ngapp/accio/src/app/app.module.ts
+++ src/main/ngapp/accio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -16,7 +17,6 @@ import { MatSelectModule } from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http'
 import { MatDialogModule } from '@angular/material/dialog';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent} from './top-bar/top-bar.component';
@@ -52,6 +52,7 @@ import { LogoutComponent } from './logout/logout.component';
     MatFormFieldModule,
     MaterialFileInputModule,
     MatInputModule,
+    MatMenuModule,
     ReactiveFormsModule,
     FormsModule,
     MatProgressSpinnerModule,
