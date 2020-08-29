767bcc45af9a2bcaeefd7a37358f44ec4eabc815
chance <laufate@gmail.com>
2020-08-29 00:39:27 +0000

    Update ts code for instant tolerance slider feedback

--- src/main/ngapp/accio/src/app/top-toolbar/top-toolbar.component.ts
+++ src/main/ngapp/accio/src/app/top-toolbar/top-toolbar.component.ts
@@ -1,5 +1,6 @@
 import { HostListener, Component, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
 
 @Component({
   selector: 'app-top-toolbar',
@@ -82,7 +83,8 @@ export class TopToolbarComponent implements OnInit {
   }
 
   /** Emits value of user inputed/slider tolerance. */
-  updateTolerance() {
+  updateTolerance(event: MatSliderChange) {
+    this.toleranceValue = event.value;
     this.newToleranceEvent.emit(this.toleranceValue);
   }
 }
