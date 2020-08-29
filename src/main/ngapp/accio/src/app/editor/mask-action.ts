da30dccdd688f03a158d8033ac08dd0d44828800
chance <laufate@gmail.com>
2020-08-29 03:36:18 +0000

    Expand mask-action to hold PreviewMask property

--- src/main/ngapp/accio/src/app/editor/mask-action.ts
+++ src/main/ngapp/accio/src/app/editor/mask-action.ts
@@ -1,5 +1,6 @@
 import { SetOperator } from './set-operator';
 import { Move } from './mask-controller.service';
+import { PreviewMask } from './magic-wand.service';
 
 export enum Action {
   ADD = 'add',
@@ -29,7 +30,8 @@ export class MaskAction {
      *   INVERT: every pixel of the image
      *   CLEAR: the pixels of the current mask
      */
-    private changedPixels: Set<number>
+    private changedPixels: Set<number>,
+    public previewMaster: PreviewMask = undefined
   ) {}
 
   public getActionType(): Action {
